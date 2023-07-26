dcc48472fb979d49023f30d1947e0eede49971ce
ADMIN-00007 <140091643+ADMIN-00007@users.noreply.github.com>
2023-07-26 14:21:01 +0200

    Update commit5

--- TrialApp/CellCityDashBoard/Form1.cs
+++ TrialApp/CellCityDashBoard/Form1.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System;
+
 namespace CellCityDashBoard
 {
     public partial class Form1 : Form
@@ -24,7 +29,7 @@ namespace CellCityDashBoard
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("https://forms.office.com/Pages/ResponsePage.aspx?id=Uxo8h1TYokeM71lH3Fj_sdQBH5zizjBImbSQ_pb3sepURExWQkY2VzlVTlhWNFZERUxQVzZGMTA0RS4u");
+            Process.Start(new ProcessStartInfo { FileName = @"https://forms.office.com/Pages/ResponsePage.aspx?id=Uxo8h1TYokeM71lH3Fj_sdQBH5zizjBImbSQ_pb3sepURExWQkY2VzlVTlhWNFZERUxQVzZGMTA0RS4u", UseShellExecute = true });
         }
 
         private void button2_Click(object sender, EventArgs e)
